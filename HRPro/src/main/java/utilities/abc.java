9ffcf8edd0f1c11048d11a056ba932b949236ef9
nehashrimal2712 <90679257+nehashrimal2712@users.noreply.github.com>
2021-09-14 19:50:54 +0530

    Update abc.java

--- HRPro/src/main/java/utilities/abc.java
+++ HRPro/src/main/java/utilities/abc.java
@@ -5,6 +5,7 @@ public class abc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println();
+		System.out.println("Hello This is for Pull example");
 		
 		
 	}
