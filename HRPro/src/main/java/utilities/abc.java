eb42c3624002b369f66ea5f26c1aea3eaa59e37d
nehashrimal2712 <Sumeet Jain@LAPTOP-6KAH58M0>
2021-09-15 01:55:29 +0530

    commit 4

--- HRPro/src/main/java/utilities/abc.java
+++ HRPro/src/main/java/utilities/abc.java
@@ -4,7 +4,7 @@ public class abc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+		System.out.println("hekekkdk");
 		System.out.println("Hello This is for Pull example");
 		
 		
